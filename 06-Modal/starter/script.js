b3b8e8cd53752eee081feaf410e89b08ab409a7a
Abhisheknayak-programer <sahilservice300@gmail.com>
2021-04-21 08:34:49 +0530

    Modified

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,21 @@
 'use strict';
+const showModel = document.querySelectorAll('.show-modal');
+const modal = document.querySelector('.modal');
+const closeModalButton = document.querySelector('.close-modal');
+const overlay = document.querySelector('.overlay');
+
+const ShowModelFunc = () => {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const CloseModalFunc = () => {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+for (let i = 0; i < showModel.length; i++) {
+  showModel[i].addEventListener('click', ShowModelFunc);
+  closeModalButton.addEventListener('click', CloseModalFunc);
+  overlay.addEventListener('click', CloseModalFunc);
+}
