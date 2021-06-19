fd370011ee002dca5a5c6a6ac1b31ebff7f8698f
Abhisheknayak-programer <sahilservice300@gmail.com>
2021-06-19 18:38:17 +0530

    map displayed on the screen

--- 15-Mapty/starter/script.js
+++ 15-Mapty/starter/script.js
@@ -10,3 +10,41 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+// Getting the coods of your current location...
+if (navigator.geolocation)
+  navigator.geolocation.getCurrentPosition(
+    function (position) {
+      // console.log(position);
+      const latitude = position.coords.latitude;
+      const longitude = position.coords.longitude;
+      // console.log(latitude,longitude)
+      console.log(`https://www.google.com/maps/@${latitude}${longitude}`);
+
+        const coords = [latitude,longitude];
+
+
+
+    // Displaying the map in th screen here 'map' which is written down is the id of the 
+    // div which will contain  the map  on the screen
+      var map = L.map('map').setView(coords, 13);
+
+      L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      L.marker(coords)
+        .addTo(map)
+        .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+        .openPopup();
+
+
+
+
+
+    },
+    function () {
+      alert('Please allow your current location 😢');
+    }
+  );
