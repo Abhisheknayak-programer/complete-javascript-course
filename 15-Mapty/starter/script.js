414ccc207ff29207f8e2696dfef7ef92271c3521
Abhisheknayak-programer <sahilservice300@gmail.com>
2021-06-20 09:31:09 +0530

    Rendering done

--- 15-Mapty/starter/script.js
+++ 15-Mapty/starter/script.js
@@ -11,6 +11,8 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
+let map, MapEvent;
+
 // Getting the coods of your current location...
 if (navigator.geolocation)
   navigator.geolocation.getCurrentPosition(
@@ -21,30 +23,57 @@ if (navigator.geolocation)
       // console.log(latitude,longitude)
       console.log(`https://www.google.com/maps/@${latitude}${longitude}`);
 
-        const coords = [latitude,longitude];
+      const coords = [latitude, longitude];
 
-
-
-    // Displaying the map in th screen here 'map' which is written down is the id of the 
-    // div which will contain  the map  on the screen
-      var map = L.map('map').setView(coords, 13);
+      // Displaying the map in th screen here 'map' which is written down is the id of the
+      // div which will contain  the map  on the screen
+      map = L.map('map').setView(coords, 13);
 
       L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution:
           '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
       }).addTo(map);
 
-      L.marker(coords)
-        .addTo(map)
-        .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-        .openPopup();
-
-
-
-
+      // Map Clicking Event
+      map.on('click', function (MapE) {
+        MapEvent = MapE;
+        form.classList.remove('hidden');
+        inputDistance.focus();
+      });
 
     },
     function () {
       alert('Please allow your current location 😢');
     }
   );
+
+
+
+// When Form Submitted the map and data manipulation functionality
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  // console.log(MapEvent.latlng.lat,MapEvent.latlng.lng);
+  const ClickedCoords = [MapEvent.latlng.lat, MapEvent.latlng.lng];
+
+  L.marker(ClickedCoords)
+    .addTo(map)
+    .bindPopup(
+      L.popup({
+        maxWidth: 250,
+        minWidth: 100,
+        autoClose: false,
+        closeOnClick: false,
+        className: `running-popup`,
+      })
+    )
+    .setPopupContent('Workout')
+    .openPopup();
+});
+
+
+
+// To toggle the Elevation gain and the Cadence
+inputType.addEventListener("change",()=>{
+  inputElevation.closest('.form__row').classList.toggle("form__row--hidden");
+  inputCadence.closest('.form__row').classList.toggle("form__row--hidden");
+})
